4d69e50f165c26843a2f9c5d421d060a22b1e42e
Julius Busch <117680351+julii-b@users.noreply.github.com>
2025-10-01 19:22:39 +0200

    docs: Add OpenAPI docs to votesRouter

--- server/src/routes/votesRouter.ts
+++ server/src/routes/votesRouter.ts
@@ -3,7 +3,48 @@ import * as voteController from '../controllers/voteConroller.js';
 
 const votesRouter = Router({ mergeParams: true });
 
+/**
+ * @openapi
+ * /answers/{answerId}/vote:
+ *   post:
+ *     summary: Cast vote for an answer
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: answerId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       201:
+ *         description: Vote casted
+ *         headers:
+ *           X-New-Token:
+ *             $ref: '#/components/headers/X-New-Token'
+ */
 votesRouter.post('/', voteController.castVote); // vote
+
+/**
+ * @openapi
+ * /answers/{answerId}/vote:
+ *   delete:
+ *     summary: Delete vote for an answer
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: answerId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       204:
+ *         description: Vote deleted
+ *         headers:
+ *           X-New-Token:
+ *             $ref: '#/components/headers/X-New-Token'
+ */
 votesRouter.delete('/', voteController.deleteVote); // delete vote
 
 export default votesRouter;
