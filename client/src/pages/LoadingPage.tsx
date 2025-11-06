e4422eaf7f879007a65493cc8201e732800a4ac2
Julius Busch <117680351+julii-b@users.noreply.github.com>
2025-11-06 15:39:42 +0100

    feat: Add styling for LoadingPage

--- client/src/pages/LoadingPage.tsx
+++ client/src/pages/LoadingPage.tsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from 'react';
+import stylesLoadingPage from './LoadingPage.module.css';
+
 const LoadingPage = () => {
+
+  // State for animated dots and effect to update them:
+  const [ dots, setDots ] = useState<string>('');
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDots((prev) => (prev.length < 6 ? prev+' .' : ''));
+    }, 200);
+    return () => clearInterval(intervalId);
+  }, []);
+
+
   return (
   <div className={`cardsContainer`} >
-    <div className={`contentCard`}>
-      loading
+    <div className={`contentCard ${stylesLoadingPage.loadingCard}`} >
+
+      <picture>
+          {/* <source srcSet="/loadingSpinner.webp" type="image/webp" />*/}
+          <img className={stylesLoadingPage.spinner}  src="/Logo.png" alt="" />
+        </picture>
+
+      <p className={stylesLoadingPage.loadingText} >
+        Loading{dots}
+      </p>
+
     </div>
   </div>
-);
+  );
 }
+
 export default LoadingPage;
